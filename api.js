34fe29d51ecd2b64580f8ca4e3115d3a57b4e7a1
ivan-berra <ivan.lorenzo.berra@gmail.com>
2018-11-26 13:59:07 +0100

    Aggiunta variabile user e verbo POST

--- api.js
+++ api.js
@@ -6,3 +6,21 @@ app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(PORT, () => console.log('Example app listening on port ' + PORT))
 
+var users = [{
+	id : 1,
+	mat : 200912,
+	email: "prova@test.com",
+	isTeacher: false,
+	}];
+
+app.post('/api/users', function (req, res) {
+	
+	var user = req.body;
+	user.id = product.length + 1;
+
+	users.push(req.body);
+
+	res.location("/api/users" + user.id);
+	res.status(201);
+	res.send();
+});
