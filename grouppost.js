bdd8c6bfe33d7da732d7615cbb28ee17b5a80364
Massimo <massimo.defacci@studenti.unitn.it>
2018-11-26 16:48:04 +0100

    ora controlla se gli utenti esistono prima di inserire il gruppo

--- grouppost.js
+++ grouppost.js
@@ -1,3 +1,6 @@
+var fs = require('fs');
+
+
 function grouppost(nomegruppo, listamembri){
 	if(typeof nomegruppo === "string")
 	{
@@ -8,10 +11,13 @@ function grouppost(nomegruppo, listamembri){
 			{
 				if(!(typeof listamembri[i] === "number" && Number.isInteger(listamembri[i])))
 					fortmaterror=true;
+				if(!fortmaterror)
+					if(!esisteuser(listamembri[i]))
+						fortmaterror=true;
 			}
 			if(!fortmaterror)
 			{
-				
+
 				return 200;
 			}
 			else return 400;
@@ -20,4 +26,37 @@ function grouppost(nomegruppo, listamembri){
 	}
 	else return 400;
 }
+
+//si suppone siano in ordine nel file
+function esisteuser(iduser)
+{
+	//var imported = require('./users.json');
+	var imported = fs.readFileSync('./users.json', 'utf8', function (err, data) {
+    if (err) throw err; // we'll not consider error handling for now
+    var obj = JSON.parse(data);
+	});
+	/*var re = /\0/g;
+	var utenti=JSON.parse(imported.toString().replace(re, ""));*/
+	var utenti = JSON.parse(imported);
+	var lookingat=iduser-1;
+	if(utenti.nextid<=iduser)
+		return false;
+	else if (utenti.groups[lookingat].id==iduser)
+		return true;
+	else {
+		var beginsearch=0;
+		var endsearch=utenti.nextid-1;
+		do{
+			if(utenti.groups[lookingat]<iduser)
+				beginsearch=lookingat+1;
+			else if (utenti.groups[lookingat]>iduser)
+				endsearch=lookingat-1;
+			else if(utenti.groups[lookingat]==iduser)
+				return true;
+				lookingat=((beginsearch+endsearch)/2);
+		}while(beginsearch>endsearch)
+		return false;
+	}
+}
+
 module.exports = grouppost;
