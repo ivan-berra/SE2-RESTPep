c7baeb46a23ab36cad89edf09c13d8a6e2e33a49
ivan-berra <ivan.lorenzo.berra@gmail.com>
2018-11-26 17:03:11 +0100

    Modificato il file per il test dell'applicazione

--- api.test.js
+++ api.test.js
@@ -1,10 +1,16 @@
-test('Tries to connect to  the server', () => {
+const url = 'https://se2-restpep-dev.herokuapp.com/';
+
+const https = require('https');
+
+test('Trying to connect to the server', () => {
+
 	var https = require("https");
+
 	var testRes;
 
 	https = require("https");
 
-	https.get('https://se2-restpep-dev.herokuapp.com/', (res) => {
+	https.get(url, (res) => {
 		
 		res.on('data', (d) => {		
 			expect(String(d)).toBe('Hello World!');
@@ -12,3 +18,35 @@ test('Tries to connect to  the server', () => {
 	});
 
 });
+
+
+/*test('Trying to create a new user through POST', () => {
+	
+	var data = JSON.stringify({
+		matricola: 200000,
+		email: "prova@prova.it",
+		isTeacher: false
+	});
+
+	var options = {
+		host: url,
+		port: 3000,
+		path: '/api/users/',
+		headers: {
+			'Content-Type': 'application/json',
+		}
+	};
+
+	var req = https.request(options, function(res) {
+	
+		res.setEncoding('utf8');
+		res.on('data', function(chunk){
+		    console.log("body: " + chunk);
+		})
+	});	
+
+	req.write(data);
+
+        
+    
+});*/
