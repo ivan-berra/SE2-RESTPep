1bf19d24d8e61f5c4dcec1a11f6f19ae153ae860
Dmitry Golyshev <fallenioshiro@gmail.com>
2018-11-26 19:19:26 +0100

    Update api.js

--- api.js
+++ api.js
@@ -1,8 +1,39 @@
 const express = require('express')
+const bodyParser = require('body-parser');
+const exampost = require('./exampost');
+
 const app = express()
 const PORT = process.env.PORT || 3000
 
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(PORT, () => console.log('Example app listening on port ' + PORT))
+var exams = [];
 
+let examsIdCounter=0;
+
+app.get('/exams', (req, res) => {
+		res.json(exams);
+})
+
+app.post('/exams', (req, res) => {
+	let newexam = req.body;
+	//taskpost guarda se i campi sono formattati bene
+	let check = exampost(newexam.destinatario, newexam.deadline, newexam.tasklist, newexam.autore, newexam.condivisi);
+  console.log(newexam);
+  console.log(check);
+	if(check==200){
+		examsIdCounter++;
+		newexam.id=examsIdCounter;
+		exams.push(newexam);
+		res.send("201 CREATED");
+	}
+	else{
+		res.send("500 BAD FORMAT");
+	}
+})
+
+app.listen(PORT, () => console.log('Example app listening on port ' + PORT))
