ead58e68ed95a9127d86bd4f489a1c1b195ef424
Massimo <massimo.defacci@studenti.unitn.it>
2018-11-26 18:54:49 +0100

    ora group/post è funzionante al 100%, modifica l'apposito json e controlla il json degli utenti

--- grouppost.js
+++ grouppost.js
@@ -6,6 +6,7 @@ function grouppost(nomegruppo, listamembri){
 	{
 		if(typeof listamembri === "object" && listamembri !=null)
 		{
+			let strtmp=[];
 			var fortmaterror = false;
 			for(var i=0; i<listamembri.length && !fortmaterror; i++)
 			{
@@ -14,10 +15,37 @@ function grouppost(nomegruppo, listamembri){
 				if(!fortmaterror)
 					if(!esisteuser(listamembri[i]))
 						fortmaterror=true;
+				if(i==listamembri.length-1)
+				{
+					//strtmp+='{userid:'+listamembri[i]+"},";
+					let numero=listamembri[i];
+					strtmp.push({"userid":numero},);
+				}
+				else
+				{
+					//strtmp+='{userid:'+listamembri[i]+"}";
+					let numero=listamembri[i];
+					strtmp.push({"userid":numero});
+				}
 			}
 			if(!fortmaterror)
 			{
+				//INSERIMENTO EFFETTIVO
+				let imported = fs.readFileSync('./groups.json', 'utf8', function (err, data) {
+			    if (err) throw err; // we'll not consider error handling for now
+			    var obj = JSON.parse(data);
+				});
+				let gruppi=JSON.parse(imported);
+				let iddaassegnare=gruppi.nextid;
+				gruppi.nextid=iddaassegnare+1;
 
+
+				//gruppi['groups'].push({"groupid":iddaassegnare,"groupname":nomegruppo,"userlist":[strtmp]});
+				gruppi['groups'].push({"groupid":iddaassegnare,"groupname":nomegruppo,"userlist":[]});
+				let exported=JSON.stringify(gruppi);
+				let index=exported.lastIndexOf("[");
+				exported=exported.substring(0,index)+JSON.stringify(strtmp)+'}'+exported.substring(index+1, exported.length-2);
+				fs.writeFileSync('./groups.json', exported);
 				return 200;
 			}
 			else return 400;
@@ -31,7 +59,7 @@ function grouppost(nomegruppo, listamembri){
 function esisteuser(iduser)
 {
 	//var imported = require('./users.json');
-	var imported = fs.readFileSync('./users.json', 'utf8', function (err, data) {
+	let imported = fs.readFileSync('./users.json', 'utf8', function (err, data) {
     if (err) throw err; // we'll not consider error handling for now
     var obj = JSON.parse(data);
 	});
@@ -41,17 +69,17 @@ function esisteuser(iduser)
 	var lookingat=iduser-1;
 	if(utenti.nextid<=iduser)
 		return false;
-	else if (utenti.groups[lookingat].id==iduser)
+	else if (utenti.users[lookingat].id==iduser)
 		return true;
 	else {
 		var beginsearch=0;
 		var endsearch=utenti.nextid-1;
 		do{
-			if(utenti.groups[lookingat]<iduser)
+			if(utenti.users[lookingat]<iduser)
 				beginsearch=lookingat+1;
-			else if (utenti.groups[lookingat]>iduser)
+			else if (utenti.users[lookingat]>iduser)
 				endsearch=lookingat-1;
-			else if(utenti.groups[lookingat]==iduser)
+			else if(utenti.users[lookingat]==iduser)
 				return true;
 				lookingat=((beginsearch+endsearch)/2);
 		}while(beginsearch>endsearch)
