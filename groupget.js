f34aa92d519de6ea27ad426b67d92d9b6cf3442c
Massimo <massimo.defacci@studenti.unitn.it>
2018-12-02 18:00:15 +0100

    Aggiunto groupget.js che restituisce tutti i gruppi

---
+++ groupget.js
@@ -0,0 +1,14 @@
+function groupget(){
+
+    let imported = fs.readFileSync('./groups.json', 'utf8', function (err, data) {
+        if (err) throw err;
+        var obj = JSON.parse(data);
+    });
+
+    let gruppi=JSON.parse(imported);
+
+    return gruppi['groups'];
+
+}
+
+module.exports = groupget;
