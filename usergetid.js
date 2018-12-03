6f85c507cefed04eff31ccf4ec45298da2d8ff95
Ivan Berra <ivan.berra@studenti.unitn.it>
2018-12-03 18:53:38 +0100

    Modificato GET(id) per essere congruente con API

--- usergetid.js
+++ usergetid.js
@@ -8,10 +8,20 @@ function usergetid(searchedId){
     {
         var searchedUser = esisteUser(searchedId);
         if (!searchedUser)
-            return 404;
-        else return 200;
+            return {"status": 404, "jsonData": null};
+        else {
+            let imported = fs.readFileSync('./users.json', 'utf8');
+	    
+            let utenti=JSON.parse(imported);
+            
+            return {
+                "status": 200,
+                "jsonData": {"id":utenti['users'][searchedId].id,"mat":utenti['users'][searchedId].mat,"email":utenti['users'][searchedId].email,"isTeacher":utenti['users'][searchedId].isTeacher}
+            };
+            
+        }
 
-    } else return 400;
+    } else return {"status":400, "jsonData": null};
 
 }
 
--- usergetid.test.js
+++ usergetid.test.js
@@ -2,36 +2,59 @@ const usergetid = require('./usergetid');
 
 test('Test valido', () => {
 
-    expect(usergetid(1)).toBe(200);
+    var received = usergetid(2); 
+
+    expect(received.status).toBe(200);
+    expect(received.jsonData.mat).toBe(137846);
+    expect(received.jsonData.email).toBe("emailuser2@legitmail.com");
+    expect(received.jsonData.isTeacher).toBe(false);
 
 });
 
 test('Test non valido: id negativo', () => {
-    expect(usergetid(-1)).toBe(400);
+
+    var received = usergetid(-1); 
+
+    expect(received.status).toBe(400);
 
 });
 
 test('Test valido: utente non esistente', () => {
-    expect(usergetid(200)).toBe(404);
+
+    var received = usergetid(200); 
+
+    expect(received.status).toBe(404);
 
 });
 
 test('Test non valido: id stringa', () => {
-    expect(usergetid("alfa")).toBe(400);
+
+    var received = usergetid("alfa"); 
+
+    expect(received.status).toBe(400);
 
 });
 
 test('Test non valido: id booleano', () => {
-    expect(usergetid(false)).toBe(400);
+    
+    var received = usergetid(false); 
+
+    expect(received.status).toBe(400);
 
 });
 
 test('Test non valido: id null', () => {
-    expect(usergetid(null)).toBe(400);
+    
+    var received = usergetid(null); 
+
+    expect(received.status).toBe(400);
 
 });
 
 test('Test non valido: id undefined', () => {
-    expect(usergetid(undefined)).toBe(400);
+    
+    var received = usergetid(undefined); 
+
+    expect(received.status).toBe(400);
 
 });
