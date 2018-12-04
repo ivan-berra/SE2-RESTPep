95616237be52378debc9f3ded61be1b8f058397a
Ivan Berra <ivan.berra@studenti.unitn.it>
2018-12-04 15:30:40 +0100

    Modificata esisteUser per ritornare id cercato

--- core/userdeleteid.js
+++ core/userdeleteid.js
@@ -2,29 +2,28 @@ var esisteUser = require("./esisteUser");
 
 var fs = require('fs');
 
-function userdeleteid(searchedId){
+function userdeleteid(searchedId) {
 
-    if (searchedId >= 0 && typeof searchedId === "number")
-    {
+    if (searchedId >= 0 && typeof searchedId === "number") {
         var searchedUser = esisteUser(searchedId);
-        if (!searchedUser)
-            return {"status": 404, "jsonData": null};
+        if (searchedUser == -1)
+            return { "status": 404, "jsonData": null };
         else {
 
             let imported = fs.readFileSync('db/users.json', 'utf8');
-	    
-            let utenti=JSON.parse(imported);
-            
-            delete utenti['users'][searchedId];
-            
-            let exported=JSON.stringify(utenti);
-	    
-	        fs.writeFileSync('db/users.json', exported);
-            
-                return {"status": 204, "jsonData": null};
+
+            let utenti = JSON.parse(imported);
+
+            delete utenti['users'][searchedUser];
+
+            let exported = JSON.stringify(utenti);
+
+            fs.writeFileSync('db/users.json', exported);
+
+            return { "status": 204, "jsonData": null };
         }
 
-    } else return {"status": 400, "jsonData": null};
+    } else return { "status": 400, "jsonData": null };
 
 }
 
