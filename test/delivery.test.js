d6d68d9ac3e14e468ae541626242e3dc149071d4
MattiaCarolo <mattia.carolo@studenti.unitn.it>
2018-12-06 11:23:41 +0100

    Bugfixed test

---
+++ test/delivery.test.js
@@ -0,0 +1,60 @@
+const delivery = require('../core/delivery');
+var examples = {
+	"id":1,
+    "soluzione":"false",
+    "punteggio":1
+}
+test('valid open question', () => {
+	var received = delivery.postdelivery(1,1,3,examples); 
+	expect(received.status).toBe(200);
+});
+
+/*test('valid open question', () => {
+	var received = delivery.getdeliveryid(1); 
+	expect(received.status).toBe(200);
+});*/
+
+test('valid open question', () => {
+	var received = delivery.getdelivery(); 
+	expect(received.status).toBe(200);
+});
+
+
+//UNVALID
+
+test('unvalid field exam_ID', () => {
+	var received = delivery.postdelivery('ciao',45,54,examples);
+	expect(received.status).toBe(400);
+});
+
+test('unvalid field tested_ID', () => {
+	var received = delivery.postdelivery(3,'ciali',34,examples);
+	expect(received.status).toBe(400);
+});
+
+test('unvalid field reviewed_ID', () => {
+	var received = delivery.postdelivery(2,45,"crisre",examples);
+	expect(received.status).toBe(400);
+});
+
+test('unvalid field examples', () => {
+	var received = delivery.postdelivery('ciao',45,54,34,examples);
+	expect(received.status).toBe(400);
+});
+
+test('multiple unvalid field ID', () => {
+	var received = delivery.postdelivery('ciao','robe',45,examples);
+	expect(received.status).toBe(400);
+});
+
+/*test('delivery not found', () => {
+	var received = delivery.getdeliveryid(-1);
+	expect(received.status).toBe(404);
+});
+
+test('unvalid ID field', () => {
+	var received = delivery.getdeliveryid("ciao come va");
+	expect(received.status).toBe(404);
+});
+*/
+
