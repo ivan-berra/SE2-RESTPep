c04fee3b82376e92f732582c9a62ee36df0b34d9
Ivan Berra <ivan.berra@studenti.unitn.it>
2018-12-06 13:02:34 +0100

    Modificati i test per lasciare inalterato il DB

--- tests/userget.test.js
+++ tests/userget.test.js
@@ -1,5 +1,21 @@
 const userget = require('../core/userget');
 
+const retreiveBackup = require('../core/retreiveBackup');
+const resetJSON = require('../core/resetJSON');
+
+const file = 'db/users.json';
+
+let fileBackup = null
+
+beforeAll(() => {
+    fileBackup = retreiveBackup(file);
+})
+
+afterEach(() => {
+    resetJSON(file, fileBackup);
+})
+
+
 test('Test valido', () => {
 
     expect(userget().status).toBe(200);
