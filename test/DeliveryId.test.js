32adfa50d65321997a66b7ca370388f1b3483ecb
Massimo <massimo.defacci@studenti.unitn.it>
2018-12-09 15:12:15 +0100

    Corretti test

--- test/DeliveryId.test.js
+++ test/DeliveryId.test.js
@@ -4,35 +4,35 @@ let deliveryJSONbackup = fs.readFileSync('./db/deliveries.json', 'utf8');
 
 
 test('valid delete', () => {
-	expect(idDelete(1).status).toBe(204);
+	expect(delivery.idDelete(1)).toBe(204);
 });
 
 test('unvalid delete', () => {
-	expect(idDelete(1).status).toBe(404);
+	expect(delivery.idDelete(1)).toBe(404);
 });
 
 test('unvalid delete', () => {
-	expect(idDelete(-7).status).toBe(400);
+	expect(delivery.idDelete(-7)).toBe(400);
 });
 
 test('unvalid delete', () => {
-	expect(idDelete(300).status).toBe(404);
+	expect(delivery.idDelete(300)).toBe(404);
 });
 
 test('unvalid delete', () => {
-	expect(idDelete(1, 7).status).toBe(400);
+	expect(delivery.idDelete(1, 7)).toBe(400);
 });
 
 test('unvalid delete', () => {
-	expect(idDelete().status).toBe(400);
+	expect(delivery.idDelete()).toBe(400);
 });
 
 test('unvalid delete', () => {
-	expect(idDelete(5.3).status).toBe(400);
+	expect(delivery.idDelete(5.3)).toBe(400);
 });
 
 test('unvalid delete', () => {
-	expect(idDelete('6').status).toBe(400);
+	expect(delivery.idDelete('6')).toBe(400);
 });
 
 fs.writeFileSync('./db/deliveries.json', deliveryJSONbackup);
